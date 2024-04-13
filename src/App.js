c7b2802348cab37f394a39ffec82056bbd43b7fa
Mr-AkashShinde <akashshinde46693@gmail.com>
2024-04-13 08:58:24 +0530

    application created

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function App() {
+  const [data, setData] = useState({});
+
+  useEffect(() => {
+    axios.get('http://localhost:5000/api/data')
+      .then(response => setData(response.data))
+      .catch(error => console.error('Error fetching data:', error));
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1>Welcome to My App</h1>
       </header>
+      <main>
+        <p>{data.message}</p>
+      </main>
+      <footer>
+        <p>Footer </p>
+      </footer>
     </div>
   );
 }
